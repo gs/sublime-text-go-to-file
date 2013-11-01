fc4811dec172c6058aa9b982f9560db8cf18f20a
stvkoch <stvkoch@gmail.com>
2013-11-01 12:47:07 +0000

    fix try to extrac class name from line

--- go_to_file.py
+++ go_to_file.py
@@ -9,23 +9,26 @@ class GoToFile(sublime_plugin.TextCommand):
             quoted_text = self.get_quoted_selection(region).split(os.sep)[-1]
             selected_text = self.get_selection(region)
             whole_line = self.get_line(region)
+            candidates = [selected_text, self.extract_candidate_from_line(), quoted_text, whole_line]
+            self.try_open(candidates)
 
-            candidates = [selected_text, quoted_text, whole_line]
-            for text in candidates:
-                if len(text) == 0:
-                    continue
+    def try_open(self, candidates):
+        for text in candidates:
+            if text is None or len(text) == 0:
+                continue
 
-                self.potential_files = self.get_filename(text)
-                if len(self.potential_files) > 0:
-                    break
+            self.potential_files = self.get_filename(text)
+            if len(self.potential_files) > 0:
+                break
 
-            if len(self.potential_files) > 1:
-                self.view.window().show_quick_panel(self.potential_files, self.open_file)
-            elif len(self.potential_files) == 1:
-                print("Opening file '%s'" % (self.potential_files[0]))
-                self.view.window().open_file(self.potential_files[0])
-            else:
-                sublime.error_message("No file found")
+        if len(self.potential_files) > 1:
+            self.view.window().show_quick_panel(self.potential_files, self.open_file)
+        elif len(self.potential_files) == 1:
+            print("Opening file '%s'" % (self.potential_files[0]))
+            self.view.window().open_file(self.potential_files[0])
+        else:
+            sublime.error_message("No file found")
+
 
     def open_file(self, selected_index):
         if selected_index != -1:
@@ -33,11 +36,14 @@ class GoToFile(sublime_plugin.TextCommand):
             print("Opening file '%s'" % (file))
             self.view.window().open_file(file)
 
+
     def get_selection(self, region):
         return self.view.substr(region).strip()
 
+
     def get_line(self, region):
         return self.view.substr(self.view.line(region)).strip()
+
 
     def get_quoted_selection(self, region):
         text = self.view.substr(self.view.line(region))
@@ -47,10 +53,12 @@ class GoToFile(sublime_plugin.TextCommand):
             quoted_text = self.expand_within_quotes(text, position, '\'')
         return quoted_text
 
+
     def expand_within_quotes(self, text, position, quote_character):
         open_quote = text.rfind(quote_character, 0, position)
         close_quote = text.find(quote_character, position)
         return text[open_quote+1:close_quote] if (open_quote > 0 and close_quote > 0) else ''
+
 
     def get_filename(self, text):
         results = []
@@ -65,10 +73,21 @@ class GoToFile(sublime_plugin.TextCommand):
                         results += [fileName]
         return results
 
+
     def walk(self, directory):
         for dir, dirnames, files in os.walk(directory):
             dirnames[:] = [dirname for dirname in dirnames]
             yield dir, dirnames, files
+
+
+    def extract_candidate_from_line(self):
+        view = sublime.active_window().active_view()
+        for sel in view.sel():
+            patternStr = view.substr(view.word(sel)).strip()
+            lineStr = view.substr(view.line(sel)).strip()
+            result = re.search( '(([^(\s|=|\+|\.)|,]*)'+patternStr+'[^(\s|:|;|,|\.|\(]*)', lineStr )
+            if result != None:
+                return result.group()
 
 class FileInfo(sublime_plugin.WindowCommand):
     def run(self):
